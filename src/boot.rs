f0a979a54e8b2d42906338f5babf37cd48e617a0
Phoebe Goldman <pgoldman@sift.net>
2020-10-20 18:00:13 -0500

    assmebly init code, which works in debug mode

--- src/boot.rs
+++ src/boot.rs
@@ -1,6 +1,4 @@
-use crate::{asm::eret, cores::{which_core, CoreNo}, core_0_main, sleep_forever};
-use cortex_a::{regs::*};
-use register::cpu::{RegisterReadOnly, RegisterReadWrite};
+use crate::{core_0_main, sleep_forever};
 
 // These are all defined in `/link.ld`
 extern "C" {
@@ -31,22 +29,30 @@ pub unsafe extern "C" fn _el2_entry() -> ! {
 
 #[link_section = ".text.boot.become_el1"]
 unsafe fn become_el1() ->! {
-    if in_el2() {
-        // put el1 in aarch64 mode
-        HCR_EL2.write(HCR_EL2::RW::EL1IsAarch64);
-        // mask all interrupts, set eret target to el1
-        SPSR_EL2.write(SPSR_EL2::D::Masked
-                           + SPSR_EL2::A::Masked
-                           + SPSR_EL2::F::Masked
-                           + SPSR_EL2::M::EL1h);
-        // set eret link to continuation
-        ELR_EL2.set(el1_entry as u64);
-        SP_EL1.set(text_start());
-        eret()
-    } else {
-        SP_EL1.set(text_start());
-        el1_entry()
-    }
+    asm!(
+        // first, initialize the el1 stack pointer
+        "adr x8, {text_start}",
+        "msr sp_el1, x8",
+        // then, if we're already in el1, go to el1_entry
+        "mrs x8, currentel",
+        "cmp w8, #0x8",
+        "b.ne {el1_entry}",
+        // otherwise, we're in el2. construct an exception from which
+        // to return.
+        // set hcr_el2 so that el1 runs in aarch64 mode
+        "mov w8, #-0x80000000",
+        "msr hcr_el2, x8",
+        // set spsr_el2 to disable interrupts and to resume into aarch64 mode
+        "mov w9, #0x345",
+        "msr spsr_el2, x9",
+        // set the exception resume address to el1_entry
+        "adr x10, {el1_entry}",
+        "msr elr_el2, x10",
+        "eret",
+        text_start = sym __text_start,
+        el1_entry = sym el1_entry,
+        options(nomem, nostack, noreturn),
+    )
 }
 
 #[link_section = ".text.boot.el1_entry"]
@@ -54,14 +60,4 @@ unsafe fn el1_entry() ->! {
     r0::zero_bss(&mut __bss_start, &mut __bss_end);
     r0::init_data(&mut __data_start, &mut __data_end, &__data_loadaddr);
     core_0_main()
-}
-
-#[inline(always)]
-fn in_el2() -> bool {
-    CurrentEL.get() == CurrentEL::EL::EL2.value
-}
-
-#[inline(always)]
-unsafe fn text_start() -> u64 {
-    (&__text_start) as *const u64 as u64
 }
