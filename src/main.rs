58be514360ff5bd5ef8bdae03364e2ee1f377682
Phoebe Goldman <pgoldman@sift.net>
2020-11-08 11:54:00 -0600

    delete some crap i wrote for debugging

--- src/main.rs
+++ src/main.rs
@@ -27,42 +27,6 @@ mod console;
 mod driver;
 mod memory;
 
-use core::default::Default;
-use core::fmt::{self, Write};
-
-struct ByteBuf {
-    buf: [u8; 1024],
-    fill: usize,
-}
-
-impl fmt::Write for ByteBuf {
-    fn write_str(&mut self, s: &str) -> fmt::Result {
-        for b in s.bytes() {
-            if self.fill == 1024 {
-                return Err(fmt::Error)
-            }
-            self.buf[self.fill] = b;
-            self.fill += 1;
-        }
-        Ok(())
-    }
-}
-
-impl Default for ByteBuf {
-    fn default() -> Self { Self {
-        buf: [0; 1024],
-        fill: 0,
-    } }
-}
-
-impl ByteBuf {
-    fn as_str(&self) -> &str { unsafe {
-        core::str::from_utf8_unchecked(
-            core::slice::from_raw_parts((&self.buf) as *const u8, self.fill)
-        )
-    } }
-}
-
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
     unsafe {
@@ -96,28 +60,7 @@ fn core_0_main() -> ! {
     console::print_str("Hello from a print_str call!\n")
         .expect("print_str failed");
 
-    console::print_str("I'm writing a whole lot in an attempt to fill up the FIFO!\n")
-        .expect("print_str failed");
-
-    {
-        let s = [b'a'; 2048];
-        console::print_str(unsafe { core::str::from_utf8_unchecked(&s) })
-            .expect("print_str failed");
-    }
-    console::print_str("\nHow cool was that?\n")
-        .expect("print_str failed");
-
-    {
-        let mut buf = ByteBuf::default();
-        buf.write_fmt(format_args_nl!("Will it work if I format into a buffer?"));
-
-        console::print_str("I wrote into a buffer!\n");
-        
-        console::print_str(buf.as_str())
-            .expect("print_str failed");
-    }
-
-    println!("This part doesn't work, though... :(");
+    println!("Hello from a println call, {}", "Phoebe");
     
     panic!("Check out this panic message!");
 }
